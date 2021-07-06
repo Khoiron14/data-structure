ecec776e38a34fec992eb1f7a1fa1db819c339ce
Muhammad NH Khoiron <muh.khoiron14@gmail.com>
2021-07-06 16:56:24 +0700

    Refactor fibonacci.kt

--- 1. Array/fibonacci.kt
+++ 1. Array/fibonacci.kt
@@ -1,11 +1,11 @@
 fun main() {
-    val fibo: Array<Int?> = arrayOfNulls(20)
+    val fibo = IntArray(20)
 
     fibo[0] = 1
     fibo[1] = 1
 
     for (i in 2..fibo.size-1) {
-        fibo[i] = fibo[i-2]!! + fibo[i-1]!!
+        fibo[i] = fibo[i-2] + fibo[i-1]
     }
 
     print("${fibo.size} Bilangan fibonacci pertama adalah : \n")
